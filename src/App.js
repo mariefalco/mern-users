6b51d1a016417e28006fdd9fbf2e9653dc20886d
Maryna Vasyliuk <mariefalco69@gmail.com>
2019-06-04 17:04:02 +0300

    Re-creation App component

--- src/App.js
+++ src/App.js
@@ -1,89 +1,28 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { authService } from "./services/authService";
-import { userService } from "./services/userService";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import Sign_in from "./components/Sign_in";
+import Registration from "./components/Registration";
+import Users from "./components/Users";
+import User from "./components/User";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: {}
-    };
-  }
+import NotFound from "./components/NotFound";
 
-  componentDidMount() {
-    authService.getToken();
-    userService
-      .getMe()
-      .then(res => {
-        this.setState({ user: res.data });
-        console.log(this.state.user);
-      })
-      .catch(error => {
-        if (error.response.status === 401) {
-          this.props.history.push("/auth/sign_in");
-        }
-      });
-  }
-
-  logout = () => authService.logout();
-
-  render() {
-    return (
+function App() {
+  return (
+    <Router>
       <div>
-        {localStorage.getItem("jwtToken") && (
-          <nav className="navbar navbar-expand navbar-dark bg-dark">
-            <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
-              <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                  <Link to="/users" className="nav-item nav-link">
-                    Users
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
-              <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                  <button
-                    class="nav-item btn btn-primary"
-                    onClick={this.logout}
-                  >
-                    Logout
-                  </button>
-                </li>
-              </ul>
-            </div>
-          </nav>
-        )}
-        <div class="container">
-          <div class="panel panel-default">
-            <div class="panel-heading">
-              <h3 class="panel-title">Welcome, {this.state.user.name}!</h3>
-            </div>
-            <div class="panel-body">
-              <div class="row">
-                <div class="col">Email</div>
-                <div class="col">{this.state.user.email}</div>
-              </div>
-              <div class="row">
-                <div class="col">Created date</div>
-                <div class="col">{this.state.user.created_date}</div>
-              </div>
-              <div class="row">
-                <div class="col">Hobby</div>
-                <div class="col">{this.state.user.hobby}</div>
-              </div>
-              <div class="row">
-                <div class="col">Responsibility</div>
-                <div class="col">{this.state.user.responsibility}</div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/auth/sign_in" component={Sign_in} />
+          <Route path="/auth/registration" component={Registration} />
+          <Route exact path="/users" component={Users} />
+          <Route path="/users/:userId" component={User} />
+          <Route path="*" component={NotFound} />
+        </Switch>
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import serviceWorker from "./serviceWorker";
 import App from "./App";
-import Sign_in from "./components/Sign_in";
-import Registration from "./components/Registration";
-import Users from "./components/Users";
-import User from "./components/User";
 
-import NotFound from "./components/NotFound";
-
-ReactDOM.render(
-  <Router>
-    <div>
-      <Switch>
-        <Route exact path="/" component={App} />
-        <Route path="/auth/sign_in" component={Sign_in} />
-        <Route path="/auth/registration" component={Registration} />
-        <Route exact path="/users" component={Users} />
-        <Route path="/users/:userId" component={User} />
-        <Route path="*" component={NotFound} />
-      </Switch>
-    </div>
-  </Router>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 serviceWorker();
