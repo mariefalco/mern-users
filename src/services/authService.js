62bfb0ce81c21c75f2f43c523d0c2895b462b90f
Maryna Vasyliuk <mariefalco69@gmail.com>
2019-06-09 10:14:26 +0300

    RefreshToken movement

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,5 +1,70 @@
 import axios from "axios";
 
+// refreshing token
+let isRefreshing = false;
+let refreshSubscribers = [];
+
+axios.interceptors.response.use(
+  response => response,
+  error => {
+    const {
+      config,
+      response: { status }
+    } = error;
+    const originalRequest = config;
+
+    if (status === 401) {
+      if (!isRefreshing) {
+        isRefreshing = true;
+        axios
+          .patch("/api/auth/refresh_token", {
+            id: localStorage.getItem("id"),
+            name: localStorage.getItem("name"),
+            refreshToken: localStorage.getItem("refreshToken")
+          })
+          .then(response => {
+            setToken(response);
+            let newToken = response.data.token;
+            isRefreshing = false;
+            onRrefreshed(newToken);
+          })
+          .catch(error => {
+            logout();
+            return Promise.reject(error);
+          });
+      }
+
+      const retryOrigReq = new Promise((resolve, reject) => {
+        subscribeTokenRefresh(token => {
+          // replace the expired token and retry
+          originalRequest.headers["Authorization"] = "Bearer " + token;
+          resolve(axios(originalRequest));
+        });
+      });
+      return retryOrigReq;
+    } else {
+      return Promise.reject(error);
+    }
+  }
+);
+
+function subscribeTokenRefresh(cb) {
+  refreshSubscribers.push(cb);
+}
+
+function onRrefreshed(token) {
+  refreshSubscribers.map(cb => cb(token));
+}
+
+// headers
+axios.interceptors.request.use(function(config) {
+  const token = localStorage.getItem("jwtToken");
+  config.headers.Authorization = token;
+
+  return config;
+});
+
+// authorization
 const isLogged = () => !!localStorage.getItem("jwtToken");
 
 const setToken = result => {
